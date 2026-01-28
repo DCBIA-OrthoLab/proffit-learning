73c7a5075ca2a6a50a6ab74e13227d742e12c7de
Paul-dumont <paul.dumont@cpe.fr>
2026-01-28 15:14:02 -0500

    redeploy avec good database name

--- netlify/functions/authenticate.js
+++ netlify/functions/authenticate.js
@@ -8,7 +8,7 @@ exports.handler = async (event) => {
     const { username, password } = JSON.parse(event.body);
 
     const client = new Client({
-        connectionString: process.env.DATABASE_URL
+        connectionString: process.env.NETLIFY_DATABASE_URL
     });
 
     try {
